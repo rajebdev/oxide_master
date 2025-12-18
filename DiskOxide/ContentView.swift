cef9b5feeb21f8ce43bf2d3629a49b4d16c6f72b
Wijanarko Putra Rajeb <wijanarko.rajeb@jatis.com>
2025-12-19 01:31:18 +0700

    fix(content-view): set default selected tab to installer

--- DiskOxide/ContentView.swift
+++ DiskOxide/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedTab: Tab = .analyzer
+    @State private var selectedTab: Tab = .installer
 
     enum Tab {
         case analyzer
